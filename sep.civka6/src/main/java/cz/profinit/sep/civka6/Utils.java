0cff01a65276bad1bbce66ce589195cb0a73703e
keschaf <keschaf@keschafs-xps>
2017-11-13 17:00:12 +0100

    javadoc

--- sep.civka6/src/main/java/cz/profinit/sep/civka6/Utils.java
+++ sep.civka6/src/main/java/cz/profinit/sep/civka6/Utils.java
@@ -5,8 +5,13 @@ import java.time.LocalDate;
 
 public class Utils {
 	private static int[] weights = new int[] {6, 3, 7, 9, 10, 5, 8, 4, 2, 1};
-//	private static int[] weights = new int[] {1, 2, 4, 8, 5, 10, 9, 7, 3, 6};
 
+	/**
+	 * ABO validator, wrong implementation. This works only for some account numbers.
+	 * @param abo account number
+	 * @return <code>true</code>, if account number is correct, <code>false</code> otherwise
+	 * @see https://www.penize.cz/bezne-ucty/15470-tajemstvi-cisla-uctu
+	 */
 	public static boolean isValidABOWrong(String abo) {
 		int sum = 0;
 		for (int i = 0; i < abo.length(); i++) {
@@ -17,6 +22,12 @@ public class Utils {
 		return (sum % 11 == 0);
 	}
 
+	/**
+	 * ABO validator, correct implementation.
+	 * @param abo account number
+	 * @return <code>true</code>, if account number is correct, <code>false</code> otherwise
+	 * @see https://www.penize.cz/bezne-ucty/15470-tajemstvi-cisla-uctu
+	 */
 	public static boolean isValidABO(String abo) {
 		int sum = 0;
 		for (int i = abo.length(); i > 0; i--) {
@@ -27,6 +38,11 @@ public class Utils {
 		return (sum % 11 == 0);
 	}
 	
+	/**
+	 * Returns <code>true</code> if given date is weekend, <code>false</code> otherwise.
+	 * @param date date to be checked
+	 * @return <code>true</code> if given date is weekend, <code>false</code> otherwise.
+	 */
 	public static boolean isWeekend(LocalDate date) {
 		DayOfWeek dayOfWeek = date.getDayOfWeek();
 		return dayOfWeek == DayOfWeek.SUNDAY || dayOfWeek == DayOfWeek.SATURDAY;
